2c075325a84ec7b4faed23ac0265688e6dc85312
Nurumbet-ux <saryevnur@gmail.com>
2020-10-15 20:20:17 +0600

    kokoko

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
+            System.out.println("______________________");
             for (int j = 1; j < 10; j++) {
                 System.out.println(i +" * " + j + " = " + i * j);
 
